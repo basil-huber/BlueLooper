f64760c593a5af5dffa2a5ad903852c628278acc
Basil Huber <basil.huber@gmail.com>
2018-10-07 23:28:31 +0200

    Added Pedal LEDs and changed LooperOSC to multithreading

--- bluelooper/bluelooper.py
+++ bluelooper/bluelooper.py
@@ -1,41 +1,43 @@
 from .bluepedal import BluePedal
 from .sooperlooper import LooperOSC
-from  functools import partial
-
-looper_state = 0
+from time import sleep
 
 
-def button_callback(looper, button_id, state):
-    global looper_state
+def set_pedal_leds(pedal, looper):
+    looper_state = looper.request_state()
+    print(looper_state)
+    if looper_state == LooperOSC.State.PLAYING:
+        pedal.setLEDState(0, BluePedal.LED_ON)
+    elif looper_state in [LooperOSC.State.RECORDING, LooperOSC.State.OVERDUBBING, LooperOSC.State.MULTIPLYING]:
+        pedal.setLEDState(0, BluePedal.LED_BLINKING)
+    else:
+        pedal.setLEDState(0, BluePedal.LED_OFF)
+
+def button_callback(looper, pedal, button_id, button_state):
+    looper_state = looper.get_state()
     if button_id == 0:
-        if state == BluePedal.RELEASED:
-            if looper_state == 0:
+        if button_state == BluePedal.RELEASED:
+            if looper_state in [LooperOSC.State.MUTED_OFF, LooperOSC.State.OFF, LooperOSC.State.UNKNOWN, LooperOSC.State.RECORDING]:
                 print("start record")
                 looper.record()
-                looper_state = 1
-            elif looper_state == 1:
-                print("stop record")
-                looper.record()
-                looper_state = 2
-            elif looper_state == 2:
-                print("start overdubbing")
+            else:
+                print("start multiplying")
                 looper.multiply()
-                looper_state = 3
-            elif looper_state == 3:
-                print("stop overdubbing")
-                looper.multiply()
-                looper_state = 2
-        elif state == BluePedal.RELEASED_LONG:
+        elif button_state == BluePedal.RELEASED_LONG:
             looper.undo()
             print("undo")
     elif button_id == 1:
-        if state == BluePedal.RELEASED:
+        if button_state == BluePedal.RELEASED:
             print("mute")
             looper.mute()
-        elif state == BluePedal.RELEASED_LONG:
+        elif button_state == BluePedal.RELEASED_LONG:
             looper.reset()
-            looper_state = 0
             print("reset looper")
+    # set pedal LEDs
+    if button_state in [BluePedal.RELEASED, BluePedal.RELEASED_LONG]:
+        sleep(0.1)
+        set_pedal_leds(pedal, looper)
+
 
 
 def main():
@@ -50,18 +52,19 @@ def main():
     #     Set up looper       #
     ###########################
     looper = LooperOSC()
-    looper.wait_for_ready()
-    looper.register_state_update()
-    print("Looper ready")
+    looper.connect()
+    looper.register_state_update() # registration does currently not work
 
     ###########################
     # Connect pedal to looper #
     ###########################
-    pedal.button_callback = partial(button_callback, looper=looper)
+    pedal.button_callback = lambda button_id, button_state: button_callback(looper, pedal, button_id,  button_state)
+    set_pedal_leds(pedal, looper)
 
     ###########################
     #       Main loop         #
     ###########################
     while True:
         pedal.waitForNotifications()
+        #looper.get_state()
 
